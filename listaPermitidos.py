889a1d09bde4b74e5ceb902c72ebca381b122a32
Luis Sustaita <ld.delacruzsustaita@ugto.mx>
2018-05-08 11:40:35 -0500

    Se agregaron personas a la lista

--- listaPermitidos.py
+++ listaPermitidos.py
@@ -5,12 +5,12 @@ class flabianos:
         self.Invitados=['Luis Sustaita','Ricardo Mirón Torres',
                'Aldo Fernando Olmeda','Paco López Ortiz','Oliver A. López',
                'MD Diaz','Cristofer Nava','Miguel Aguirre','Polo A. Ruiz','Rodolfo Ferro',
-               'Juan Manuel Rocha',' Ernesto Ramírez de Sancristobal',
-               'Adolfo Ramírez de Sancristobal','Omar Jair Purata Funes']
+               'Juan Manuel Rocha','Ernesto Ramírez de Sancristobal',
+               'Adolfo Ramírez de Sancristobal','Omar Jair Purata Funes',
+               'Anthony Stark','Daniel PV','Jonathan Ivan']
 
-    def TuSiTuNo(self,EllosSi):
-        for person in EllosSi:
-            if person in self.Invitados:
-                print('Bienvenido {}'.format(person))
-            else:
-                print('Lo siento {}, aun no trais el omnitrix'.format(person))
+    def TuSiTuNo(self,EllosSi):        
+        if EllosSi in self.Invitados:
+            print('Bienvenido {}'.format(EllosSi))
+        else:
+            print('Lo siento {}, aun no trais el omnitrix'.format(EllosSi))
